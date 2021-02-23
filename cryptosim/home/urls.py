faf8679b72ed2d7940f3d123ec2261e9de5c82c9
Ali <farahmand.1990@gmail.com>
2021-02-23 16:10:32 -0500

    signin update

--- cryptosim/home/urls.py
+++ cryptosim/home/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", auth_views.LoginView.as_view(template_name='signin.html'), name="login"),
-    path("login", auth_views.LogoutView.as_view(template_name='signin.html'), name="logout"),
+    path("logout", auth_views.LogoutView.as_view(template_name='signin.html'), name="logout"),
+    path("dashboard", views.dashboard, name="dashboard"),
     path("register", views.register, name="register"),
 ]
--- cryptosim/home/views.py
+++ cryptosim/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 def index(request):
     return render(request, "index.html")
@@ -8,14 +10,30 @@ def index(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def LoginView(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('dashboard')
+    else:
+        return redirect('register')
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
